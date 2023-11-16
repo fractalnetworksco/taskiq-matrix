843e30c65947be4df6abe875cf1b4eb6139256c3
Jonathan <145945985+jleal-esu@users.noreply.github.com>
2023-11-16 15:31:47 -0500

    Test task (#4)
    * WIP starting test_task.py from scratch. Original file never made it into main
    
    * recreated the test_task.py file and added tests

---
+++ tests/queue/test_task.py
@@ -0,0 +1,50 @@
+from taskiq_matrix.matrix_queue import Task, AckableMessage
+import json
+import pytest
+
+async def test_task_verify_constructor():
+    """
+    Test that the data passed to the Task constructor matches what is in the task
+    object.
+    """
+
+    # create a task event dictionary to use as a parameter for a Task object
+    task_event = {
+        "body": {
+            "task_id": "1",
+            "task": json.dumps({"data": "Test Task 1"}),
+            "queue": "mutex",
+        },
+        "msgtype": "matrix_queue.task_types.ack"
+    }
+
+    # create a Task object
+    test_task = Task(**task_event)
+    
+    # verify that the information created locally matches what is 
+    # returned in the task object
+    assert test_task.id == task_event["body"]["task_id"]
+    assert test_task.type == task_event["msgtype"]
+    assert test_task.data == json.loads(task_event["body"]["task"])
+    assert test_task.queue == task_event["body"]["queue"]
+
+async def test_task_yield_task_raise_error():
+    """
+    Test that calling yield_task() raises a NotImplementedError
+    """
+
+    # create a task event dictionary to use as a parameter for a Task object
+    task_event = {
+        "body": {
+            "task_id": "1",
+            "task": json.dumps({"data": "Test Task 1"}),
+            "queue": "mutex",
+        },
+        "msgtype": "matrix_queue.task_types.ack"
+    }
+
+    # create a Task object
+    test_task = Task(**task_event)
+
+    with pytest.raises(NotImplementedError):
+        await test_task.yield_task()
