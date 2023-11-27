fbea06323764e42fcce4d9468f7b2ee42ab1b521
Jonathan <jonathan.leal1231@gmail.com>
2023-11-27 17:58:53 -0500

    worked on tests in schedule source

--- tests/schedulesource/test_matrix_room_schedule_source.py
+++ tests/schedulesource/test_matrix_room_schedule_source.py
@@ -1,6 +1,7 @@
 from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
+from taskiq import ScheduledTask
 
 from taskiq_matrix.schedulesource import MatrixRoomScheduleSource
 
@@ -16,11 +17,11 @@ async def test_matrix_room_schedule_constructor_error():
         test_schedule = MatrixRoomScheduleSource(broker)
 
 
+@pytest.mark.integtest  # depends on parent class' startup function
 async def test_matrix_room_schedule_startup(test_matrix_broker):
     """
     Tests that the startup() function sets the schedule's 'initial' property
     to True
-    ~~~add to spreadsheet~~~
     """
 
     broker = await test_matrix_broker()
@@ -36,11 +37,11 @@ async def test_matrix_room_schedule_startup(test_matrix_broker):
     assert test_schedule.initial == True
 
 
+@pytest.mark.integtest  # depends on parent class' startup function
 async def test_matrix_room_schedule_get_schedules_true_initial(test_matrix_broker):
     """
     Test that get_schedules() returns an empty list if the schedule's 'initial' property
     is set to True
-    ~~~add to spreadsheet~~~
     """
 
     broker = await test_matrix_broker()
@@ -56,6 +57,7 @@ async def test_matrix_room_schedule_get_schedules_true_initial(test_matrix_broke
     test_schedule.get_schedules_from_room.assert_not_called()
 
 
+@pytest.mark.integtest  # depends on parent class' startup function
 async def test_matrix_room_schedule_get_schedules_no_tasks(test_matrix_broker):
     """
     Test that an epty list is returned if there are no tasks in the schedule
@@ -71,7 +73,6 @@ async def test_matrix_room_schedule_get_schedules_no_tasks(test_matrix_broker):
     ) as mock_get_schedules_from_room, patch.object(
         test_schedule.broker, "get_all_tasks", new_callable=AsyncMock
     ) as mock_get_all_tasks:
-
         mock_get_schedules_from_room.return_value = []
         mock_get_all_tasks.return_value = {}
 
@@ -81,6 +82,8 @@ async def test_matrix_room_schedule_get_schedules_no_tasks(test_matrix_broker):
         test_schedule.get_schedules_from_room.assert_called_once()
         test_schedule.broker.get_all_tasks.assert_called_once()
 
+
+@pytest.mark.integtest  # depends on Exception class
 async def test_matrix_room_schedule_get_schedules_non_existant_task(test_matrix_broker):
     """
     Tests that the function raises an exception if the task name is not in broker_tasks
@@ -89,10 +92,14 @@ async def test_matrix_room_schedule_get_schedules_non_existant_task(test_matrix_
     test_schedule = MatrixRoomScheduleSource(broker)
     test_schedule.initial = False
 
-    with patch.object(test_schedule, "get_schedules_from_room", new_callable=AsyncMock) as mock_get_schedules_from_room:
+    with patch.object(
+        test_schedule, "get_schedules_from_room", new_callable=AsyncMock
+    ) as mock_get_schedules_from_room:
         mock_get_schedules_from_room.return_value = [{"name": "non_existent_task", "labels": {}}]
 
-        with pytest.raises(Exception, match="Got schedule for non-existant task: non_existent_task"):
+        with pytest.raises(
+            Exception, match="Got schedule for non-existant task: non_existent_task"
+        ):
             await test_schedule.get_schedules()
 
             mock_get_schedules_from_room.assert_called_once()
@@ -100,23 +107,40 @@ async def test_matrix_room_schedule_get_schedules_non_existant_task(test_matrix_
 
 async def test_matrix_room_schedule_get_schedules_broker_check(test_matrix_broker):
     """
-    Test the behavior of the line: 
+    Test the behavior of the line:
     if broker_tasks[task["name"]].broker != self.broker: continue
     ! this test went horribly, come back to this one
     """
 
+
+@pytest.mark.integtest  # depends on Exception and get_schedules_from_room
 async def test_matrix_room_schedule_get_schedules_no_cron_or_time(test_matrix_broker):
     """
+    Tests that an error is raised if the task doesn not have "cron" or "time" key-value
+    pairs
+
+    ! not recognizing the broker's get_all_tasks function
     """
-    with patch("taskiq_matrix.schedulesource.MatrixRoomScheduleSource.get_schedules_from_room", new_callable=AsyncMock) as mock_get_schedules_from_room:
-        task_with_missing_schedule_no_cron_time = {"name": "task_with_missing_schedule_no_cron_time", "labels": {}}
+    with patch(
+        "taskiq_matrix.schedulesource.MatrixRoomScheduleSource.get_schedules_from_room",
+        new_callable=AsyncMock,
+    ) as mock_get_schedules_from_room:
+        task_name = "task_with_missing_schedule_no_cron_time"
+        task_with_missing_schedule_no_cron_time = {
+            "name": task_name,
+            "labels": {},
+        }
         mock_get_schedules_from_room.return_value = [task_with_missing_schedule_no_cron_time]
 
-        broker = await test_matrix_broker()  
-        test_schedule = MatrixRoomScheduleSource(broker)
-        test_schedule.initial = False
+        with patch.object(test_matrix_broker, 'get_all_tasks', return_value={task_name: MagicMock()}):
+            broker = await test_matrix_broker()
+            test_schedule = MatrixRoomScheduleSource(broker)
+            test_schedule.initial = False
 
-        with pytest.raises(Exception) as e:
-            await test_schedule.get_schedules()
-
-        assert str(e.value) == "Schedule for task task_with_missing_schedule_no_cron_time has no cron or time"
+            with pytest.raises(Exception) as e:
+                await test_schedule.get_schedules()
+                print("this is the error: ", str(e.value))
+                assert (
+                    str(e.value)
+                    == f"Schedule for task {task_name} has no cron or time"
+                )
