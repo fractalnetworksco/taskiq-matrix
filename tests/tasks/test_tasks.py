2e1a89601df99e1d20bb27402d37e51e2e9229f4
Jonathan <jonathan.leal1231@gmail.com>
2023-12-01 14:30:57 -0500

    only one test left to finish for tasks.py, can't get the queue to recognize acked tasks

--- tests/tasks/test_tasks.py
+++ tests/tasks/test_tasks.py
@@ -3,7 +3,7 @@ import os
 from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
-from nio import RoomContextResponse
+from nio import RoomContextError
 
 from taskiq_matrix.filters import run_sync_filter
 from taskiq_matrix.matrix_queue import MatrixQueue
@@ -18,9 +18,12 @@ async def test_update_checkpoint_attribute_error():
     that doesn't match an existing queue.
     """
 
+    # create a queue name expected error message
     queue_name = "test"
     expected_error = f"Queue {queue_name} does not exist"
 
+    # patch the getattr function and force it to return an Attribute error and raise
+    # a QueueDoesNotExist exception
     with patch("taskiq_matrix.tasks.getattr", MagicMock(side_effect=AttributeError())):
         with pytest.raises(QueueDoesNotExist, match=expected_error):
             await update_checkpoint(queue_name)
@@ -32,10 +35,13 @@ async def test_update_checkpoint_no_tasks():
     asserting that get_first_unacked_task() is not called.
     """
 
+    # patch the get_first_unacked_task() function
     with patch(
         "taskiq_matrix.tasks.get_first_unacked_task", new_callable=AsyncMock
     ) as mock_unacked_task:
         await update_checkpoint("mutex")
+
+        # verify that get_first_unacked_task() was not called
         mock_unacked_task.assert_not_called()
 
 
@@ -44,20 +50,24 @@ async def test_update_checkpoint_unacked_task(test_matrix_broker):
     Test that context.start is used as the new checkpoint if there is an unacked task
     ! find a way to check context.start to verify
     """
-    broker = await test_matrix_broker()
-    await broker.startup()
 
+    # create a broker from fixture
+    broker = await test_matrix_broker()
+
+    # patch the broker used in the function with the broker from the fixture
     with patch("taskiq_matrix.tasks.broker", broker) as test_broker:
         matrix_queue: MatrixQueue = test_broker.mutex_queue
 
         await matrix_queue.checkpoint.get_or_init_checkpoint()
 
+        # create an event dictionary
         event2 = {
             "task_id": "kdjfosdf-4j239034",
             "queue": "mutex",
             "msgtype": matrix_queue.task_types.task,
         }
 
+        # send the event to the queue
         await send_message(
             matrix_queue.client,
             matrix_queue.room_id,
@@ -68,28 +78,36 @@ async def test_update_checkpoint_unacked_task(test_matrix_broker):
         )
 
 
+        # call update_checkpoint()
         await update_checkpoint("mutex")
 
+    # shut the broker down
     await broker.shutdown()
 
 
 async def test_update_checkpoint_acked_task(test_matrix_broker):
     """ 
+    ! having issues picking up acknowledged tasks
+    ! WIP
     """
-    broker = await test_matrix_broker()
-    await broker.startup()
 
+    # create a broker from fixture
+    broker = await test_matrix_broker()
+
+    # patch the broker used in the function with the broker from the fixture
     with patch("taskiq_matrix.tasks.broker", broker) as test_broker:
         matrix_queue: MatrixQueue = test_broker.mutex_queue
 
         checkpoint = await matrix_queue.checkpoint.get_or_init_checkpoint()
 
+        # create an acknowledged event dictionary
         event2 = {
             "task_id": "josdfj09b48907w3",
             "queue": "mutex",
             "msgtype": f"{matrix_queue.task_types.ack}.josdfj09b48907w3",
         }
 
+        # send the event to the queue
         await send_message(
             matrix_queue.client,
             matrix_queue.room_id,
@@ -99,8 +117,61 @@ async def test_update_checkpoint_acked_task(test_matrix_broker):
             task_id=event2["task_id"],
         )
 
+        print('room id==========', matrix_queue.room_id)
         
+        # task_id = "kdjfosdf-4j239034"
+
+        # await matrix_queue.ack_msg(task_id)
+
+        # tasks = await matrix_queue.get_tasks(timeout=0)
+        # print(tasks)
 
         await update_checkpoint("mutex")
 
+    await broker.shutdown()
+
+async def test_update_checkpoint_context_error(test_matrix_broker):
+    """
+    Tests that an exception is raised if room_context() returns a RoomContextError
+    """
+
+    # create a broker from fixture
+    broker = await test_matrix_broker()
+
+    # patch the broker used in the function with the broker from the fixture
+    with patch("taskiq_matrix.tasks.broker", broker) as test_broker:
+        matrix_queue: MatrixQueue = test_broker.mutex_queue
+
+        await matrix_queue.checkpoint.get_or_init_checkpoint()
+
+        # create an unacknowledged event dictionary
+        event2 = {
+            "task_id": "kdjfosdf-4j239034",
+            "queue": "mutex",
+            "msgtype": matrix_queue.task_types.task,
+        }
+
+        # send the event to the queue
+        await send_message(
+            matrix_queue.client,
+            matrix_queue.room_id,
+            message=json.dumps(event2),
+            queue="mutex",
+            msgtype=event2["msgtype"],
+            task_id=event2["task_id"],
+        )
+
+        # mock the room_context and have it return a RoomContextError
+        mock_context = AsyncMock()
+        mock_context.return_value = RoomContextError(message="test message", status_code="abc")
+        matrix_queue.client.room_context = mock_context
+        
+        # catch the exception raised by the RoomContextError
+        with pytest.raises(Exception) as e:
+            await update_checkpoint("mutex")
+            
+        # verify that the RoomContextError messsage is present in the exception raised
+        assert 'test message' in str(e.value)
+
+    # shut the broker down
     await broker.shutdown()
