f61e190f28b2270a12935db50252ec9cf4e2397e
Mo Balaa <balaa@fractalnetworks.co>
2024-01-10 11:42:20 -0600

    Add pytest-benchmark example

--- tests/lock/test_matrix_lock.py
+++ tests/lock/test_matrix_lock.py
@@ -1,5 +1,6 @@
 import json
 from base64 import b64encode
+from typing import Awaitable, Callable
 from unittest.mock import (
     AsyncMock,
     MagicMock,
@@ -7,10 +8,13 @@ from unittest.mock import (
     create_autospec,
     patch,
 )
+from uuid import uuid4
 
 import pytest
 from fractal.matrix.async_client import FractalAsyncClient
-from nio import MatrixRoom, RoomMessagesResponse, SyncResponse
+from nio import AsyncClient, MatrixRoom, RoomMessagesResponse, SyncResponse
+from taskiq_matrix.exceptions import LockAcquireError
+from taskiq_matrix.filters import create_filter, run_sync_filter
 from taskiq_matrix.lock import (
     LockAcquireError,
     MatrixLock,
@@ -609,3 +613,57 @@ async def test_matrix_lock_get_latest_sync_token_good_response(new_matrix_room):
     result = await lock.get_latest_sync_token()
     assert result == "test token"
     MatrixLock.next_batch = None
+
+
+async def test_matrix_lock_acquired(
+    matrix_client: AsyncClient, new_matrix_room: Callable[[], Awaitable[str]], aio_benchmark
+):
+    """
+    Ensure that a lock can be acquired, and if it is acquired,
+    it can't be acquired again.
+    """
+    test_room_id = await new_matrix_room()
+
+    @aio_benchmark
+    async def test():
+        import time
+
+        time.sleep(1)
+        # generate a unique key to lock on
+        key = str(uuid4())
+        async with MatrixLock(room_id=test_room_id).lock(key) as lock_id:
+            # verify that lock is acquired
+            res = await run_sync_filter(
+                matrix_client,
+                create_filter(test_room_id, types=[f"fn.lock.acquire.{key}"]),
+                timeout=0,
+            )
+            assert res[test_room_id][0]["msgtype"] == f"fn.lock.acquire.{key}"
+            assert json.loads(res[test_room_id][0]["body"])["lock_id"] == lock_id
+
+
+async def test_matrix_lock_acquired_no_reacquire(
+    matrix_client: AsyncClient, new_matrix_room: Callable[[], Awaitable[str]]
+):
+    """
+    Ensure that a lock can be acquired, and if it is acquired,
+    it can't be acquired again.
+    """
+    # generate a unique key to lock on
+    key = str(uuid4())
+    test_room_id = await new_matrix_room()
+
+    async with MatrixLock(room_id=test_room_id).lock(key) as lock_id:
+        # verify that lock is acquired
+        res = await run_sync_filter(
+            matrix_client,
+            create_filter(test_room_id, types=[f"fn.lock.acquire.{key}"]),
+            timeout=0,
+        )
+        assert res[test_room_id][0]["msgtype"] == f"fn.lock.acquire.{key}"
+        assert json.loads(res[test_room_id][0]["body"])["lock_id"] == lock_id
+
+        with pytest.raises(LockAcquireError):
+            # attempting to acquire the lock again should fail since it's already acquired
+            async with MatrixLock(room_id=test_room_id).lock(key):
+                assert False  # should never get here
